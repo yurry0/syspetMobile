246b9a114ea2eded159c716fed45ad56ace41ea6
Yuri <yurimartinsr@gmail.com>
2021-12-07 15:12:31 -0300

    Posição de FAB trocada para melhorar usabilidade

--- src/screens/ClientesIndex.js
+++ src/screens/ClientesIndex.js
@@ -22,7 +22,7 @@ export default class ClientesIndex extends Component {
         };
     }
 
-  
+
     //
     componentDidMount() {
         let db;
@@ -56,12 +56,12 @@ export default class ClientesIndex extends Component {
         );
     };
 
- 
+
     render() {
 
         return (
 
-            
+
 
             <View style={{ flex: 1 }}>
 
@@ -70,79 +70,81 @@ export default class ClientesIndex extends Component {
                     <Text style={styles.textoCab}>Clientes</Text>
                     <Text>---------------------------------------------</Text>
                     <Text style={{ justifyContent: 'center', textAlign: 'center', fontSize: 15, marginBottom: 4, color: 'white' }}>Toque em um item da lista para obter mais detalhes de cada cliente</Text>
-
-                    <TouchableOpacity onPress={() => {Actions.refresh({key: Math.random()});}}>
+                    <View style={{flexDirection: 'row'}}>
+                    <ActionButton
+                    style={{marginTop: 20}}
+                        offsetY={10}
+                        offsetX={0}
+                        position="center"
+                        buttonColor="rgba(41,167,45,1)"
+                        onPress={() => { Actions.ClientesCadastrar() }}
+                    />
+                    <TouchableOpacity style={{marginLeft: 250}} onPress={() => { Actions.refresh({ key: Math.random() }); }}>
                         <Icon name="sync" size={35} style={{ paddingBottom: 25, color: 'white' }}> </Icon>
                     </TouchableOpacity>
-
+                    </View>
                 </View>
-                    <FlatList
-                        data={this.state.FlatListItems}//data recebe as informações buscadas na tabela cliente
-                        ItemSeparatorComponent={this.ListViewItemSeparator}
-                        keyExtractor={(item, index) => index.toString()}
-                        renderItem={({ item }) => (
-                            <View
-                                key={item.id}
-                                style={{ backgroundColor: 'white', padding: 30, borderBottomWidth: 1, flexDirection: 'row', justifyContent: 'space-between' }}
-                            >
-                                <View>
+                <FlatList
+                    data={this.state.FlatListItems}//data recebe as informações buscadas na tabela cliente
+                    ItemSeparatorComponent={this.ListViewItemSeparator}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <View
+                            key={item.id}
+                            style={{ backgroundColor: 'white', padding: 30, borderBottomWidth: 1, flexDirection: 'row', justifyContent: 'space-between' }}
+                        >
+                            <View>
 
-                                    <TouchableOpacity onPress={() => { Actions.ClientesView({ id: item.pk_id_cliente, cli_nome: item.cli_nome, cli_cep: item.cli_cep, cli_endereco: item.cli_endereco, cli_bairro: item.cli_bairro, cidade: item.cidade, cli_estado: item.cli_estado, cli_rg: item.cli_rg, cli_email: item.cli_email }) }}>
-                                        <Text>Id: {item.pk_id_cliente}</Text>
-                                        <Text>Nome: {item.cli_nome}</Text>
-                                    </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { Actions.ClientesView({ id: item.pk_id_cliente, cli_nome: item.cli_nome, cli_cep: item.cli_cep, cli_endereco: item.cli_endereco, cli_bairro: item.cli_bairro, cidade: item.cidade, cli_estado: item.cli_estado, cli_rg: item.cli_rg, cli_email: item.cli_email }) }}>
+                                    <Text>Id: {item.pk_id_cliente}</Text>
+                                    <Text>Nome: {item.cli_nome}</Text>
+                                </TouchableOpacity>
 
-                                </View>
-                                <View style={{ justifyContent: 'flex-end', flexDirection: 'row' }}>
-
-                                    <View style={{ marginRight: 5 }}>
-                                        <Button style={styles.botao} icon={
-                                            <Icon
-                                                name="create-outline"
-                                                size={15}
-                                                color="white"
-                                            />
-                                        } buttonStyle={{ backgroundColor: 'orange' }} title='Alterar' onPress={() => {
-                                            Alert.alert('Alterar:', 'Deseja alterar o registro de ' + item.cli_nome + '?',
-                                                [
-                                                    { text: 'Sim', onPress: () => { Actions.ClientesEditar({ id: item.pk_id_cliente, cli_nome: item.cli_nome, cli_cep: item.cli_cep, cli_endereco: item.cli_endereco, cli_bairro: item.cli_bairro, cidade: item.cidade, cli_estado: item.cli_estado, cli_rg: item.cli_rg, cli_email: item.cli_email, estado: 'editar' }) } },
-                                                    { text: 'Não' }
-
-                                                ])
-
-                                        }} />
-                                    </View>
-                                    <View>
-                                        <Button style={styles.botao} icon={
-                                            <Icon
-                                                name="trash-outline"
-                                                size={15}
-                                                color="white"
-                                            />
-                                        } buttonStyle={{ backgroundColor: 'red' }} title='Excluir' onPress={() => {
-                                            Alert.alert('Excluir:', 'Deseja excluir o registro de ' + item.cli_nome,
-                                                [
-                                                    { text: 'Sim', onPress: () => { this.deletar(item.pk_id_cliente) } },
-                                                    { text: 'Não', onPress: () => console.log('Não') }
-                                                ]);
-                                        }} />
-                                    </View>
-
-
-
-                                </View>
                             </View>
+                            <View style={{ justifyContent: 'flex-end', flexDirection: 'row' }}>
 
-                        )}
-                    />
+                                <View style={{ marginRight: 5 }}>
+                                    <Button style={styles.botao} icon={
+                                        <Icon
+                                            name="create-outline"
+                                            size={15}
+                                            color="white"
+                                        />
+                                    } buttonStyle={{ backgroundColor: 'orange' }} title='Alterar' onPress={() => {
+                                        Alert.alert('Alterar:', 'Deseja alterar o registro de ' + item.cli_nome + '?',
+                                            [
+                                                { text: 'Sim', onPress: () => { Actions.ClientesEditar({ id: item.pk_id_cliente, cli_nome: item.cli_nome, cli_cep: item.cli_cep, cli_endereco: item.cli_endereco, cli_bairro: item.cli_bairro, cidade: item.cidade, cli_estado: item.cli_estado, cli_rg: item.cli_rg, cli_email: item.cli_email, estado: 'editar' }) } },
+                                                { text: 'Não' }
 
-                <ActionButton
-                    offsetY={25}
-                    offsetX={0}
-                    position="center"
-                    buttonColor="rgba(231,76,60,1)"
-                    onPress={() => { Actions.ClientesCadastrar() }}
+                                            ])
+
+                                    }} />
+                                </View>
+                                <View>
+                                    <Button style={styles.botao} icon={
+                                        <Icon
+                                            name="trash-outline"
+                                            size={15}
+                                            color="white"
+                                        />
+                                    } buttonStyle={{ backgroundColor: 'red' }} title='Excluir' onPress={() => {
+                                        Alert.alert('Excluir:', 'Deseja excluir o registro de ' + item.cli_nome,
+                                            [
+                                                { text: 'Sim', onPress: () => { this.deletar(item.pk_id_cliente) } },
+                                                { text: 'Não', onPress: () => console.log('Não') }
+                                            ]);
+                                    }} />
+                                </View>
+
+
+
+                            </View>
+                        </View>
+
+                    )}
                 />
+
+
 
 
             </View>
